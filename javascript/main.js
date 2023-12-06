1a68463265f2720734ed7be330caa869f268756c
sophietran03 <sophietran03@gmail.com>
2023-12-05 21:49:09 -0700

    docs: add If-else statement

--- javascript/main.js
+++ javascript/main.js
@@ -25,5 +25,13 @@ const counterText = document.getElementById('txt-counter');
 counterButton.onclick = () => {
     count = count + 1;
     counterText.innerHTML = `Number: ${count}`;
-};
 
+    // If-else statement 
+    if (count % 2 == 0) {
+        counterText.classList.remove('odd');
+        counterText.classList.add('even');
+    } else {
+        counterText.classList.remove('even');
+        counterText.classList.add('odd');
+    }
+};
